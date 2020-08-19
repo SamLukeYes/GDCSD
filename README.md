6abb4767051ce09897735ef4ff38062ff583c090
SamLukeYes <samlukeyes123@gmail.com>
2020-08-19 23:11:03 +0800

    add feature

--- README.md
+++ README.md
@@ -2,23 +2,19 @@
 A program aiming to accelerate Geometry Dash custom song downloading using Aria2
 
 ## How to use
-Before running GDCSD, please make sure you have installed [Python](https://python.org) (tested on 3.7 and 3.8) and [Aria2](https://aria2.github.io). If you're using Windows, you might need to add them to PATH.
+**NOTE: dev branch may contain incomplete features. Please only use master branch, except for development use.**
 
-For the first time to run or after an upgrade, please execute:
+Before running GDCSD, please make sure you have installed [Python](https://python.org) (tested on 3.7 and 3.8) and [Aria2](https://aria2.github.io). If you're using Windows, you might need to add the absolute paths of `aria2c.exe` and `python.exe` to `PATH`.
 
-    chmod +x ./gdcsd.py  #for non-Windows users
-    ./gdcsd.py init
+    USAGE: python gdcsd.py <command> [args]
+        
+    Defined commands:
 
-Then the Python script will install `gdcsd.sh` and `controller.py` to `~/.local/share/gdcsd`. On Windows, there'll be `gdcsd.cmd` and `controller.py` in the default download directory of GD instead.
-
-The shell script will be invoked for downloading and executing shell commands. I expect it to run out of box, but I haven't tested it on other devices, so please check them before downloading. If it doesn't work out of box on your device, please open an issue to discuss it.
-
-    USAGE: ./gdcsd.py <command> [args]
-    
-    Commands:
-
-        init            Set up the runtime environment
-        dl              Download songs by their IDs and store them in GD's custom song directory
+        info            Display copyright infomation and current download directory
+        dl              Download songs by ID and store them in custom song download directory
+        set             Set custom song download directory, recommended to be the same as GD's
+        reset           Reset custom song download directory to default
+        clean           Clean song info cache
         help            Display this message
         
-        Other commands: Run a shell command in GD's custom song directory (not available on Windows)
+    Other commands: Execute a system command in custom song download directory.
--- gdcsd.py
+++ gdcsd.py
@@ -23,15 +23,17 @@ import os, platform, time, sys
 
 home = os.path.expanduser('~')
 dl_config = f'{home}/.gdcsd_dl_dir'
-tmp_dir = f'{home}/.cache/gdcsd'
+cache_dir = f'{home}/.cache/gdcsd'
+tmp_dir = f'{cache_dir}/tmp'
 
-if os.path.isfile(dl_config):   # if not exist, fall back to default
-    dl_dir = open(dl_config).read()
-elif platform.system() == 'Windows':
-    dl_dir = f'{home}/AppData/Local/GeometryDash'
-else:
-    # for any other platform that uses Proton to run GD
-    dl_dir = f'{home}/.local/share/Steam/steamapps/compatdata/322170/pfx/drive_c/users/steamuser/Local Settings/Application Data/GeometryDash'
+def get_dl_dir():
+    if os.path.isfile(dl_config):   # if not exist, fall back to default
+        return open(dl_config).read()
+    elif platform.system() == 'Windows':
+        return f'{home}/AppData/Local/GeometryDash'
+    else:
+        # for any other platform that uses Proton to run GD
+        return f'{home}/.local/share/Steam/steamapps/compatdata/322170/pfx/drive_c/users/steamuser/Local Settings/Application Data/GeometryDash'
 
 def usage():
     print(f'''USAGE: {sys.argv[0]} <command> [args]
@@ -40,10 +42,12 @@ Defined commands:
 
     info            Display copyright infomation and current download directory
     dl              Download songs by ID and store them in custom song download directory
-    set-dl-dir      Set custom song download directory, recommended to be the same as GD's
+    set             Set custom song download directory, recommended to be the same as GD's
+    reset           Reset custom song download directory to default
+    clean           Clean song info cache
     help            Display this message
     
-Other commands: Execute a system command in GD's custom song directory.
+Other commands: Execute a system command in custom song download directory.
 ''')
 
 def set_dl_dir(dir:str):
@@ -51,7 +55,7 @@ def set_dl_dir(dir:str):
         print(f'Invalid path: {dir}')
         sys.exit(1)
     path = os.path.abspath(dir)
-    print(f'Writing to {dl_config}')
+    #print(f'Writing to {dl_config}')
     with open(dl_config, 'w') as f:
         f.write(path)
     print(f'Custom song download directory set to {path}')
@@ -68,74 +72,92 @@ def check_dl_dir():
         print(f"Please run '{sys.argv[0]} set-dl-dir <YOUR_GD_CUSTOM_SONG_PATH>'")
         sys.exit(1)
 
-def mkdir(dir:str):
-    if platform.system() == 'Windows':
-        return os.system(f'powershell mkdir -p "{dir}"')
-    else:
-        return os.system(f'mkdir -p "{dir}"')
+def mkdir(target_dir:str):
+    if not os.path.isdir(target_dir):
+        if platform.system() == 'Windows':
+            return os.system(f'powershell mkdir -p "{target_dir}"')
+        else:
+            return os.system(f'mkdir -p "{target_dir}"')
+    return 0
 
-def rmdir(dir:str):
+def rmdir(target_dir:str):
     if platform.system() == 'Windows':
-        return os.system(f'rmdir /s /q "{dir}"')
+        return os.system(f'rmdir /s /q "{target_dir}"')
     else:
-        return os.system(f'rm -rf "{dir}"')
+        return os.system(f'rm -rf "{target_dir}"')
 
-def multi_dl(targets:dict, ctrl_file:str, outdir:str):
+def multi_dl(targets:dict, outdir:str):
+    mkdir(tmp_dir)
+    ctrl_file = f'{tmp_dir}/{time.time()}'
     ctrl = ''
     for file_name in targets:
         ctrl += f'{targets[file_name]}\n out={file_name}\n'
     with open(ctrl_file, 'w') as f:
         f.write(ctrl)
-    return os.system(f'aria2c -c -d "{outdir}" -i "{ctrl_file}"')
+    code = os.system(f'aria2c -c -d "{outdir}" -i "{ctrl_file}"')
+    os.remove(ctrl_file)
+    return code
 
 def dl(IDs:list):
 
-    # Notes:
-    #   For invoking, always use sys.exit()
-    #   For editing this func itself, always rmdir(work_dir) before returning
+    # NOTE: always use sys.exit() when invoking
 
     check_dl_dir()
-    work_dir = f'{tmp_dir}/{time.time()}'
-    mkdir(work_dir)
-    os.chdir(work_dir)
+    # work_dir = f'{tmp_dir}/{time.time()}'
+    # mkdir(work_dir)
+    # os.chdir(work_dir)
 
     print('Fetching song info...')
     api_targets = dict()
+    existing = os.listdir(cache_dir) if os.path.isdir(cache_dir) else []
     for id in IDs:
-        if id.isdigit():
+        if id in existing:
+            continue
+        elif id.isdigit():
             api_targets[id] = f'https://api.newgrounds.app/details.php?url=https://www.newgrounds.com/audio/listen/{id}'
         else:
             raise ValueError(f'song ID {id} is invalid')
-    code = multi_dl(api_targets, 'api.tmp', work_dir)
+    code = multi_dl(api_targets, cache_dir)
     if code:
         print('Failed to fetch song info.')
-        rmdir(work_dir)
+        #rmdir(work_dir)
         return code
     
     print('Starting download...')
     mp3_targets = dict()
     for id in IDs:
         url = None
-        data = open(id).read().split()
+        data = open(f'{cache_dir}/{id}').read().split()
         for i in range(len(data)):
             if data[i] == '"media":':
                 url = data[i+1].replace('"', '').replace('\\', '')
         if url:
             mp3_targets[f'{id}.mp3'] = url
         else:
-            print(f'Failed to get download URL of {id}. Removing it from target list.')
-            print(f"If you're sure it is a valid ID, please open an issue at {home_url}/issues")
+            print(f'Failed to get download URL of {id}. Skipping.')
+            print(f"Maybe the cache is corrupted. Try '{sys.argv[0]} clean' to fix it.")
+            print(f"If it doesn't help, feel free to open an issue at {home_url}/issues")
             time.sleep(3)
-    code = multi_dl(mp3_targets, 'songs.tmp', dl_dir)
-    rmdir(work_dir)
+    code = multi_dl(mp3_targets, dl_dir)
+    #rmdir(work_dir)
     return code
+
+def clean():
+    rmdir(cache_dir)
+
+def reset():
+    if os.path.isfile(dl_config):
+        os.remove(dl_config)
+    print(f'Custom song download directory set to {get_dl_dir()}')
 
 def info():
     print(copyright_msg)
     print(f'Home page: {home_url}\n')
-    print(f'Custom song download path: {dl_dir}\n')
+    print(f'Custom song download directory: {dl_dir}\n')
 
 if __name__ == '__main__':
+
+    dl_dir = get_dl_dir()
 
     if len(sys.argv) < 2:
         usage()
@@ -150,11 +172,17 @@ if __name__ == '__main__':
             sys.exit(1)
         sys.exit(dl(sys.argv[2:]))
 
-    elif sys.argv[1] == 'set-dl-dir':
+    elif sys.argv[1] == 'set':
         if len(sys.argv) == 2:
-            print(f'USAGE: {sys.argv[0]} set-dl-dir <YOUR_GD_CUSTOM_SONG_PATH>')
+            print(f'USAGE: {sys.argv[0]} set-dl-dir <CUSTOM_SONG_PATH>')
             sys.exit(1)
         set_dl_dir(sys.argv[2])
+
+    elif sys.argv[1] == 'clean':
+        clean()
+
+    elif sys.argv[1] == 'reset':
+        reset()
 
     elif sys.argv[1] == 'help':
         usage()
