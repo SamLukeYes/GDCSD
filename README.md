3d7e5af5ef82f2626ae0459e162ae42621b32fab
SamLukeYes <samlukeyes123@gmail.com>
2020-08-19 20:26:31 +0800

    rewrite (partly)

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 A program aiming to accelerate Geometry Dash custom song downloading using Aria2
 
 ## How to use
-Before running GDCSD, please make sure you have installed [Python](https://python.org) (3.6 or higher) and [Aria2](https://aria2.github.io) properly.
+Before running GDCSD, please make sure you have installed [Python](https://python.org) (tested on 3.7 and 3.8) and [Aria2](https://aria2.github.io). If you're using Windows, you might need to add them to PATH.
 
 For the first time to run or after an upgrade, please execute:
 
@@ -22,31 +22,3 @@ The shell script will be invoked for downloading and executing shell commands. I
         help            Display this message
         
         Other commands: Run a shell command in GD's custom song directory (not available on Windows)
-
-## Device for testing
-    System:
-    Kernel: 5.8.1-arch1-2-surface x86_64 bits: 64 compiler: N/A 
-    parameters: initrd=\intel-ucode.img initrd=\initramfs-linux-surface.img 
-    root=LABEL=root rw 
-    Desktop: GNOME 3.36.5 tk: GTK 3.24.22 wm: gnome-shell dm: GDM 3.36.3 
-    Distro: Manjaro Linux 
-    Machine:
-    Type: Laptop System: Microsoft product: Surface Pro 6 
-    v: 124000000000000000000000D:0B:13F:5C:10P:38S:01E:0 serial: <filter> 
-    Chassis: type: 9 serial: <filter> 
-    Mobo: Microsoft model: Surface Pro 6 serial: <filter> UEFI: Microsoft 
-    v: 235.3192.768 date: 03.24.2020 
-
-~~My Windows 10 was broken and I don't plan to reinstall it, so... this program is not yet tested on real Windows. In addition, I haven't managed to make Aria2 work in Wine, so it is not fully tested with Wine either.~~
-
-I've also set up a Windows 10 VM with GNOME Boxes for testing, but I failed to run GD in the VM, so I can only test without GD in it.
-
-## Missing features
-* GUI
-* Checking duplicated song
-* Checking if the download directory is correct
-* Downloading songs which don't have direct link, such as *Starchaser (ID: 689891)* -- it can be downloaded in the latest Geometry Dash, but I don't know how RubRub solved this problem
-* Executing DOS commands on Windows
-* Mobile device support
-  
-These features might be added if someone really needs them. New ideas and pull requests are also welcomed.
--- gdcsd.py
+++ gdcsd.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 copyright_msg = '''Geometry Dash Custom Song Downloader
 Copyright (c) 2020 Sam L. Yes
@@ -17,160 +17,151 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 '''
 
-import sys, os, platform, time
+home_url = "https://github.com/SamLukeYes/GDCSD"
+
+import os, platform, time, sys
 
 home = os.path.expanduser('~')
-#install_path = f'{home}/.local/share/gdcsd'
-url = 'https://newgrounds.com/audio/download/'
+dl_config = f'{home}/.gdcsd_dl_dir'
+tmp_dir = f'{home}/.cache/gdcsd'
 
-if platform.system() == 'Windows':
-    script_type = 'cmd'
-    #install_path = home
+if os.path.isfile(dl_config):   # if not exist, fall back to default
+    dl_dir = open(dl_config).read()
+elif platform.system() == 'Windows':
     dl_dir = f'{home}/AppData/Local/GeometryDash'
-    install_path = dl_dir
 else:
-    script_type = 'sh'
-    install_path = f'{home}/.local/share/gdcsd'
+    # for any other platform that uses Proton to run GD
     dl_dir = f'{home}/.local/share/Steam/steamapps/compatdata/322170/pfx/drive_c/users/steamuser/Local Settings/Application Data/GeometryDash'
 
-# def detect_downloader():
-
-#     for downloader in ['aria2c', 'axel', 'curl', 'wget']:
-#         if os.system(f'{downloader} --version') == 0:
-#             return downloader
-
-def create_controller(controller):
-    with open(controller, 'w') as script:
-        script.write(
-f'''#!/usr/bin/python3
-from sys import argv
-for id in argv[1:]:
-    print('{url}/'+id)
-    print(" out="+id+".mp3")'''
-    )
-
-def install():
-
-    controller = f'{install_path}/controller.py'
-
-    if platform.system() == 'Windows':
-        os.system(f'mkdir /p "{install_path}"')
-        with open(f'{install_path}/gdcsd.{script_type}', 'w') as script:
-            script.write(f'python {controller} %* > "{install_path}/ngdl.lst"\naria2c -c -d {dl_dir} -i "{install_path}/ngdl.lst"')
-        #os.system(f'setx path %path%;{install_path}')
-        create_controller(controller)
-
-    else:
-        os.system(f'mkdir -p "{install_path}"')
-        os.chdir(install_path)
-        with open(f'gdcsd.{script_type}', 'w') as script:
-            script.write(f'#!/bin/sh\ndl_dir="{dl_dir}"\n{controller} $@ | aria2c -c -d "$dl_dir" -i -')
-        create_controller(controller)
-        os.system('chmod +x *')
-
-def init():
-
-    #downloader = detect_downloader()
-    print(copyright_msg)
-
-    #dl_dir = 'Local Settings/Application Data/GeometryDash'
-    
-    #if platform.system() == 'Windows':
-        #dl_dir = f'{home}/{dl_dir}'
-        #script_type = 'cmd'
-    #else:
-        #dl_dir = f'{home}/.local/share/Steam/steamapps/compatdata/322170/pfx/drive_c/users/steamuser/{dl_dir}'
-        #script_type = 'sh'
-
-    install()
-    print('Checking if Aria2 is available...')
-    time.sleep(1)
-
-    if os.system('aria2c -v'):
-        print(f'Aria2 not detected! Please edit {install_path}/gdcsd.{script_type} manually.')
-        #found_aria2 = False
-    #else:
-        #found_aria2 = True
-
-    print(f'\nDownload directory is set to {dl_dir}')
-    print(f'If it is incorrect, please edit {install_path}/gdcsd.{script_type} manually.')
-
-
-def how_to_use():
+def usage():
     print(f'''USAGE: {sys.argv[0]} <command> [args]
     
-Commands:
+Defined commands:
 
-    init            Set up the runtime environment
-    dl              Download songs by ID and store them in GD's custom song directory
+    info            Display copyright infomation and current download directory
+    dl              Download songs by ID and store them in custom song download directory
+    set-dl-dir      Set custom song download directory, recommended to be the same as GD's
     help            Display this message
     
-    Other commands: Run a shell command in GD's custom song directory (not available on Windows)
-    ''')
-    #sys.exit(1)
+Other commands: Execute a system command in GD's custom song directory.
+''')
 
-def dl(args):
+def set_dl_dir(dir:str):
+    if not os.path.isdir(dir):
+        print(f'Invalid path: {dir}')
+        sys.exit(1)
+    path = os.path.abspath(dir)
+    print(f'Writing to {dl_config}')
+    with open(dl_config, 'w') as f:
+        f.write(path)
+    print(f'Custom song download directory set to {path}')
 
-    argstr = ''
-    for id in args:
+def join_args(arg_list:list):
+    args = ''
+    for arg in arg_list:
+        args += arg + ' '
+    return args[:-1]
+
+def check_dl_dir():
+    if not os.path.isdir(dl_dir):
+        print(f"Error: {dl_dir} not exist")
+        print(f"Please run '{sys.argv[0]} set-dl-dir <YOUR_GD_CUSTOM_SONG_PATH>'")
+        sys.exit(1)
+
+def mkdir(dir:str):
+    if platform.system() == 'Windows':
+        return os.system(f'powershell mkdir -p "{dir}"')
+    else:
+        return os.system(f'mkdir -p "{dir}"')
+
+def rmdir(dir:str):
+    if platform.system() == 'Windows':
+        return os.system(f'rmdir /s /q "{dir}"')
+    else:
+        return os.system(f'rm -rf "{dir}"')
+
+def multi_dl(targets:dict, ctrl_file:str, outdir:str):
+    ctrl = ''
+    for file_name in targets:
+        ctrl += f'{targets[file_name]}\n out={file_name}\n'
+    with open(ctrl_file, 'w') as f:
+        f.write(ctrl)
+    return os.system(f'aria2c -c -d "{outdir}" -i "{ctrl_file}"')
+
+def dl(IDs:list):
+
+    # Notes:
+    #   For invoking, always use sys.exit()
+    #   For editing this func itself, always rmdir(work_dir) before returning
+
+    check_dl_dir()
+    work_dir = f'{tmp_dir}/{time.time()}'
+    mkdir(work_dir)
+    os.chdir(work_dir)
+
+    print('Fetching song info...')
+    api_targets = dict()
+    for id in IDs:
         if id.isdigit():
-            argstr += f'{id} '
+            api_targets[id] = f'https://api.newgrounds.app/details.php?url=https://www.newgrounds.com/audio/listen/{id}'
         else:
-            #print(f'Invalid argument: {id}\n')
-            #how_to_use()
-            #sys.exit(1)
-            raise ValueError(f'invalid song ID {id}')
+            raise ValueError(f'song ID {id} is invalid')
+    code = multi_dl(api_targets, 'api.tmp', work_dir)
+    if code:
+        print('Failed to fetch song info.')
+        rmdir(work_dir)
+        return code
+    
+    print('Starting download...')
+    mp3_targets = dict()
+    for id in IDs:
+        url = None
+        data = open(id).read().split()
+        for i in range(len(data)):
+            if data[i] == '"media":':
+                url = data[i+1].replace('"', '').replace('\\', '')
+        if url:
+            mp3_targets[f'{id}.mp3'] = url
+        else:
+            print(f'Failed to get download URL of {id}. Removing it from target list.')
+            print(f"If you're sure it is a valid ID, please open an issue at {home_url}/issues")
+            time.sleep(3)
+    code = multi_dl(mp3_targets, 'songs.tmp', dl_dir)
+    rmdir(work_dir)
+    return code
 
-    exit_code = os.system(f'{install_path}/gdcsd.{script_type} {argstr}')
-    if exit_code:
-        
-        #sys.exit(exit_code)
-        return exit_code
-
-    for id in args:
-        target = f'{dl_dir}/{id}.mp3'
-        try:
-            with open(target) as f:
-                f.read()
-            #os.remove(target)
-            print(f'{id}.mp3 doesn\'t seem to be a valid mp3 file.')
-            print(f'Try visiting https://newgrownds.com/audio/listen/{id} to download it manually.')
-        except UnicodeDecodeError:
-            pass
-
-    return 0
+def info():
+    print(copyright_msg)
+    print(f'Home page: {home_url}\n')
+    print(f'Custom song download path: {dl_dir}\n')
 
 if __name__ == '__main__':
 
     if len(sys.argv) < 2:
-        how_to_use()
+        usage()
         sys.exit(1)
 
-    elif sys.argv[1] == 'init':
-        init()
-
-    elif sys.argv[1] in ('help', '-h', '--help', '-?' '/help', '/h', '/?'):
-        how_to_use()
-
-    elif not os.path.isfile(f'{install_path}/gdcsd.{script_type}'):
-        print(f'Please run "{sys.argv[0]} init" first.')
+    elif sys.argv[1] == 'info':
+        info()
 
     elif sys.argv[1] == 'dl':
-        if dl(sys.argv[2:]):
-            print(f'Command failed for unknown reason. Run "{sys.argv[0]} init" to fix it?')
+        if len(sys.argv) == 2:
+            print(f'USAGE: {sys.argv[0]} dl <ID1> [<ID2> <ID3> ...]')
+            sys.exit(1)
+        sys.exit(dl(sys.argv[2:]))
 
-    elif platform.system == 'Windows':
-        print('Custom command is not yet available on Windows.')
+    elif sys.argv[1] == 'set-dl-dir':
+        if len(sys.argv) == 2:
+            print(f'USAGE: {sys.argv[0]} set-dl-dir <YOUR_GD_CUSTOM_SONG_PATH>')
+            sys.exit(1)
+        set_dl_dir(sys.argv[2])
+
+    elif sys.argv[1] == 'help':
+        usage()
 
     else:
-        with open(f'{install_path}/gdcsd.sh') as script:
-            target = 'dl_dir='
-            for line in script:
-                if line.startswith(target):
-                    dl_dir = line[len(target):-1].replace('"', '')
-                    #break
+        check_dl_dir()
         os.chdir(dl_dir)
-        cmd = ''
-        for arg in sys.argv[1:]:
-            cmd += f'{arg} '
-        sys.exit(os.system(cmd))
+        code = os.system(join_args(sys.argv[1:]))
+        if code:
+            print(f"Run '{sys.argv[0]} help' for help message.")
